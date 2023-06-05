e0c6d7fdf05546c61d96a764f6dcd3ca3e55f694
NightLab <rosyiqnurilhidayat@gmail.com>
2023-06-05 10:17:50 +0700

    e

--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost"; // Ganti dengan host database Anda
-$username = "root"; // Ganti dengan username database Anda
-$password = ""; // Ganti dengan password database Anda
-$dbname = "web"; // Ganti dengan nama database Anda
+$servername = "localhost"; // Ganti dengan host database 
+$username = "root"; // Ganti dengan username database 
+$password = ""; // Ganti dengan password database 
+$dbname = "web"; // Ganti dengan nama database 
 
 // Membuat koneksi ke database MariaDB
 $conn = new mysqli($servername, $username, $password, $dbname);
