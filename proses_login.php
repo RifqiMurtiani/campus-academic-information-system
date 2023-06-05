b236d8effe8ad2b4abdbcb37d359cbe7b5eeff61
NightLab <rosyiqnurilhidayat@gmail.com>
2023-06-06 01:53:04 +0700

    new

--- proses_login.php
+++ proses_login.php
@@ -1,36 +1,37 @@
 <?php
-include 'koneksi.php';
 session_start();
+include 'koneksi.php';
 if(isset($_POST["login"])){
-        $user = $_POST['username'];    
-        $password = $_POST['password'];
+    $user = $_POST['username'];    
+    $password = $_POST['password'];
         $sql = mysqli_query($conn, "SELECT * FROM user WHERE username = '$user' && password = '$password'");
         $search = mysqli_num_rows($sql);
+        // $data = mysqli_fetch_assoc($sql);
+        // echo $data['role'];
             if($search > 0){
                 $data = mysqli_fetch_assoc($sql);
                 if($data['role']==1){
                     //admin
                     $_SESSION['username'] = $username;
                     $_SESSION['role'] = 1;
-                    header("location:dashboard_admin.html");
+                    header("location:dashboar.html");
                 }else if($data['role']==2){
                     //dosen
                     $_SESSION['username'] = $username;
                     $_SESSION['role'] = 2;
             
-                    header("location:dashboard_dosen.html");
-                }
+                    header("location:dashboard.html");
                 }else if($data['role']==3){
                     //mahasiswa
                     $_SESSION['username'] = $username;
                     $_SESSION['role'] = 3;
             
-                    header("location:dashboard_mhs.html");
+                    header("location:dashboard.html");
                 }else{
-            
                     header("location:index.php?pesan=gagal");
                 }	
             }else{
                 header("location:index.php?pesan=gagal");
             }
+        }
 ?> 
